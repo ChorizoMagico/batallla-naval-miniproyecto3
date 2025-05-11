d9a10fc63e114a455554a0cc0d17a1a9b1da7875
ChorizoMagico <juanestebanarias.2006@gmail.com>
2025-05-11 09:34:48 -0500

    Functional home stage

--- src/main/java/com/example/batallanaval/model/Boats.java
+++ src/main/java/com/example/batallanaval/model/Boats.java
@@ -1,4 +1,18 @@
 package com.example.batallanaval.model;
 
+import javafx.scene.Group;
+
+import java.util.ArrayList;
+
 public class Boats {
+
+    private Group aircraftCarrier;
+    private ArrayList<Group> submarines;
+    private ArrayList<Group> destructors;
+    private ArrayList<Group> frigates;
+
+    public void createAircraftCarrier(){
+        aircraftCarrier = new Group();
+    }
+
 }
