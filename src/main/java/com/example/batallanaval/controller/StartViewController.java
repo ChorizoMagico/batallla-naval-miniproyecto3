52e222ddcc33682309f076c6f7dae3805dbf1b6e
LaspriUV <jorge.lasprilla@correounivalle.edu.co>
2025-05-11 10:51:46 -0500

    Create instructions-view and InstructionsController

--- src/main/java/com/example/batallanaval/controller/StartViewController.java
+++ src/main/java/com/example/batallanaval/controller/StartViewController.java
@@ -2,9 +2,14 @@ package com.example.batallanaval.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class StartViewController {
 
@@ -40,7 +45,23 @@ public class StartViewController {
     }
 
     private void handleInstructions (ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/batallanaval/FXML/instructions-view.fxml"));
+            Parent root = fxmlLoader.load();
 
+            // Accedemos al controlador y le pasamos la escena anterior para poder volver
+            InstructionsController instructionsController = fxmlLoader.getController();
+            Stage currentStage = (Stage) instructionsButton.getScene().getWindow();
+            instructionsController.setMainScene(currentStage.getScene());
+
+            //Mostramos la nueva scene
+            Scene instructionScene = new Scene(root);
+            currentStage.setScene(instructionScene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Se termino la excepecion.");
+        }
     }
 
     private void handleExit (ActionEvent actionEvent) {
