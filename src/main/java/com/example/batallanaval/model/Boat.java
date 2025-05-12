7ad656a23ae430fd1f8d457be98f1f2f97c2f302
CarpinchoBellako <aragon.steven@correounivalle.edu.co>
2025-05-11 23:41:32 -0500

    logicBoard update 0.1

--- src/main/java/com/example/batallanaval/model/Boat.java
+++ src/main/java/com/example/batallanaval/model/Boat.java
@@ -1,13 +1,32 @@
 package com.example.batallanaval.model;
 
 public class Boat {
-
-    private final int resistance;
     private final int size;
+    private int resistance;
+    private final boolean isHorizontal;
 
-    // Creacion de constructor
-    public Boat(int size, int resistance) {
+    public Boat(int size, int resistance, boolean isHorizontal) {
         this.size = size;
         this.resistance = resistance;
+        this.isHorizontal = isHorizontal;
     }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getResistance() {
+        return resistance;
+    }
+
+    public boolean getIsHorizontal() {
+        return isHorizontal;
+    }
+
+    public void reduceResistance() {
+        if (resistance > 0) {
+            resistance--;
+        }
+    }
+
 }
