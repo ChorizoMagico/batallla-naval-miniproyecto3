9689de39463c3f6f6a39b5669ab35c13bc5fa4fe
CarpinchoBellako <aragon.steven@correounivalle.edu.co>
2025-07-30 16:01:53 -0500

    javadocupdate 2.0

--- src/main/java/exceptions/ShipsNotPlacedException.java
+++ src/main/java/exceptions/ShipsNotPlacedException.java
@@ -1,5 +1,8 @@
 package exceptions;
-
+/**
+ * Custom checked exception thrown when ships have not been placed before starting the game.
+ * This exception extends {@link Exception} and provides a default message indicating the issue.
+ */
 public class ShipsNotPlacedException extends Exception {
     public ShipsNotPlacedException() {
         super("No se han colocado barcos.");
